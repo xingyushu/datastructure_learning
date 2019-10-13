2905f509452817eb3db6c6aedcb3de76658bba2f
xingyushu <1402598162@qq.com>
2019-10-13 11:28:18 +0800

    update grammar

--- go/basic/grammar/struct/stuct1.go
+++ go/basic/grammar/struct/stuct1.go
@@ -8,7 +8,16 @@ type  Book struct{
 }
 
 
+type Person struct{
+	Name string 
+	Age int  
+	Address  string  
+}
 
+type Interval struct{
+	Start int 
+	End int 
+}
 
 func changeBook(book Book) string{
 	book.Title = "book1"
@@ -29,5 +38,15 @@ func main(){
 	book1.Nums = 1000
 	changeBook2(&book1)
 	fmt.Println(book1)
+
+	p :=&Person{"Mike",23,"Beijing"}
+	fmt.Println(p)
+	var p2 Person
+	p2 = Person{"Bob",23,"Shenzhen"}
+
+	fmt.Println(p2)
+	inter :=Interval{Start:2,End:4}
+	fmt.Println(inter)
+
 
 }
